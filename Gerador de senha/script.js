1dc104de59fb8eb142d5dcc115c7d2e39405988e
Pedro Braga <110943497+PedroBraga0@users.noreply.github.com>
2024-12-16 21:21:30 -0300

    Add files via upload

---
+++ Gerador de senha/script.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+    const length = document.getElementById('length').value; // Obtém o comprimento da senha
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+<>?";
+    let password = "";
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+  
+    document.getElementById('password').textContent = password; // Exibe a senha gerada
+  }
+  
